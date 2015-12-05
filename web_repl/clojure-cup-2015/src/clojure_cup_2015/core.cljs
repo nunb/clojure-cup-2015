ac8c55d1a34b95fba528d2db00ff5382f3561ccd
marwa <marwa@appsnsites.com>
2015-12-05 17:58:48 +0100

    init editor with clojurescript + handling events fo getting content

--- web_repl/clojure-cup-2015/src/clojure_cup_2015/core.cljs
+++ web_repl/clojure-cup-2015/src/clojure_cup_2015/core.cljs
@@ -1,38 +1,71 @@
 (ns clojure-cup-2015.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]]))
+            [cljs.core.async :refer [<!]])
+  )
 
-(def parenode-api "http://localhost:3000/parenode/convert")
 
 (enable-console-print!)
-
-(println "Edits to this text should show up in your developer console.")
+(.log js/console "============ WELCOME TO PARENODE CONSOLE =====================")
 
 ;; define your app data so that it doesn't get over-written on reload
-
+(def validate-button (.getElementById js/document "validate"))
+(def parenode-api "http://localhost:3000/parenode/convert")
+(def codemirror-config {"value"           (.-innerHTML (.getElementById js/document "default-template")),
+                        "mode"            "scheme",
+                        "readOnly"        false,
+                        "styleActiveLine" true,
+                        "lineNumbers"     true,
+                        })
 (defonce app-state (atom {:text "Hello world!"}))
 
 
 (defn convert-scheme [expression]
   (let [response (http/post parenode-api
                             {:with-credentials? false
-                             :json-params {:expression expression}})]
-
-
-    (let [script (response :status)] )
+                             :json-params       {:expression expression}})]
     (print response)
     response)
-    "")
+  "")
 
 (defn render-script [script, root-div]
-  (let [element (d/string-to-dom (str "<script></script>")
+  let [element (d/string-to-dom (str "<script></script>"))]
   )
 
-(convert-scheme "(def varA \"test\"")
+;(convert-scheme "(def varA \"test\"")
 
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn create-editor [config]
+  (js/CodeMirror (.getElementById js/document "scheme-codemirror") (clj->js config)))
+
+; Content manipulation methods
+(defn get-value
+  ([editor] (.getValue editor))
+  ([editor separator] (.getValue editor separator))
+
+  )
+
+; Cursor and selection methods
+(defn get-selection
+  [editor]
+  (.getSelection editor))
+
+(defn get-cursor
+  [editor]
+  (.getCursor editor))
+
+(defn get-expression []
+  (println (.-line (get-cursor editor)))
+  (println (.-ch (get-cursor editor)))
+  (println (get-selection editor))
+  )
+
+(defn parenode-reload-hook []
+  (set! (.-onclick validate-button)
+        ;#(println (.-line(get-cursor editor)))
+        #(get-expression)
+        ))
+
+
+;; Initialization
+(def editor (create-editor codemirror-config))
+(parenode-reload-hook)
