9621bfe30ae95f3388ed801580195f9345be4ee5
Rafik Naccache <rafik@tnteam.rocks>
2015-12-05 22:34:14 +0000

    define-syntax seems to be working... needs more testing

--- parenode/.cljs_node_repl/node_repl.js
+++ parenode/.cljs_node_repl/node_repl.js
@@ -3,7 +3,7 @@ process.env.NODE_DISABLE_COLORS = true;
 var net  = require("net");
 var vm   = require("vm");
 var dom  = require("domain").create();
-var PORT = 49492;
+var PORT = 53835;
 
 try {
     require("source-map-support").install();
--- parenode/src/parenode/macros.clj
+++ parenode/src/parenode/macros.clj
@@ -38,6 +38,10 @@
                                                     [:else `(scheme->cljs ~op)]
                                                     `[(scheme->cljs ~cnd) (scheme->cljs ~op)]))
                                                 conditions))
+
+         [(['define-syntax syntax-name (['syntax-rules literals pattern-templates] :seq)] :seq)] `(define-syntax-macro ~syntax-name
+                                                                                                    ~literals
+                                                                                                    ~pattern-templates) 
          
          [(['begin & exprs] :seq)] (cons `do (scheme-body->cljs exprs))
          
@@ -49,28 +53,65 @@
 
          [:default] `(:error in ~exp)))
 
+(defn ellipsis-seq
+  [a_seq]
+  (if (= '... (last a_seq)) (into '(& $$ellipsis$$)
+                                  (pop (reverse a_seq)))
+      a_seq))
+
 (defn change-tpl-seq
   [ a_seq]
   ;; there must be only one ellipsis for the tpl to be good!!
   ;; if there are many of them, the will be matched against the same pattern!!
-  (cond (seq? a_seq) (let [a_seq_modified (if (= '... (last a_seq)) (into '(& $$ellipsis$$)
-                                                                        (pop (reverse a_seq)))
-                                              a_seq)]
+  (cond (seq? a_seq) (let [a_seq_modified (ellipsis-seq a_seq)]
                        `([~@a_seq_modified] :seq))
 
         :else a_seq))
+
+(defn  remove-ellipsis
+  [a-seq]
+  (clojure.walk/postwalk #(if (and (seq? %)(= '... (last %)))
+                            (into '((eval $$ellipsis$$))
+                                  (pop (reverse %)))
+                            %)  
+                            a-seq))
 
 (defn scheme-literals->keywords
   [literals a_seq]
   (cond
     (some #{a_seq} literals) (keyword a_seq)
-    :else a_seq))
+    :else  a_seq))
 
 (defn scheme-tpl->cljs
   [pattern literals]
   (->> pattern
        (clojure.walk/postwalk (comp (partial scheme-literals->keywords literals)
-                                    change-tpl-seq literals))))
+                                    change-tpl-seq))))
 
+(defn define-syntax-match-row
+  [literals pattern template]
+  `([~(scheme-tpl->cljs pattern literals)] (scheme->cljs  ~(remove-ellipsis template)) ))
 
+(defn define-syntax-matches
+  [literals pattern-templates]
+  (let [pattern-templates-pairs (partition 2 2 pattern-templates)]
+    (mapcat
+     (fn [[p t]]
+       (let [inspected-pattern-in (pop p)
+             inspected-pattern (if (= 1 (count inspected-pattern-in))
+                                 (first inspected-pattern-in)
+                                 inspected-pattern-in)]
+         (define-syntax-match-row literals inspected-pattern t)))
+     pattern-templates-pairs)))
 
+(defmacro define-syntax-macro
+  [macro-name
+   literals
+   pattern-templates]
+  (let [input (gensym 'input)
+        pattern-rows (define-syntax-matches literals  pattern-templates)]
+    `(defmacro ~macro-name
+       [~input]
+       (let [input-lit-kws# (scheme-literals->keywords ~literals ~input) ]
+         (match [input-lit-kws#]
+                ~@pattern-rows)))))
