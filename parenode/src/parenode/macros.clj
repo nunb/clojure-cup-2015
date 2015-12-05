63373a4fa6c8d9d459e7124a6c31847b9163926d
Rafik Naccache <rafik@tnteam.rocks>
2015-12-05 15:56:24 +0000

    fixed some corner cases with symbols

--- parenode/src/parenode/macros.clj
+++ parenode/src/parenode/macros.clj
@@ -1,10 +1,9 @@
 (ns parenode.macros
   (:require [clojure.core.match :refer [match]]))
 
-
-
 (defmacro scheme->cljs [exp]
   (match [exp]
+         [a_symbol]  a_symbol
          [(['car alist] :seq)] `(first (scheme->cljs ~alist))
          [(['cdr alist] :seq)] `(rest (scheme->cljs  ~alist))
          [(['cons elt alist] :seq)] `(cons (scheme->cljs ~elt) (scheme->cljs ~alist))
@@ -12,8 +11,7 @@
                                                          (fn [statement] `(scheme->cljs ~statement))
                                                          body))
          [(['define target binding] :seq)] `(def ~target (scheme->cljs ~binding)) 
-         [(['if cond do_true do_false] :seq)] `(if (scheme->cljs @cond) (scheme->cljs ~do_true) (scheme->cljs ~do_false))
          [(['quote an_exp] :seq)] `'~an_exp
          [([proc & args] :seq)] (cons `(scheme->cljs ~proc)  (map (fn [arg] `(scheme->cljs ~arg)) args))
-         [a_symbol]  a_symbol
+         [([] :seq)] nil
          [:default] :error))
