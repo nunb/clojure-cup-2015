fcf254ed70fcd0a9a882660f5ef678f298929df1
Rafik Naccache <rafik@tnteam.rocks>
2015-12-05 18:34:50 +0000

    added begin

--- parenode/src/parenode/macros.clj
+++ parenode/src/parenode/macros.clj
@@ -10,6 +10,7 @@
 
 
 (defmacro scheme->cljs [exp]
+
   (match [exp]
 
          [(false :<< seq?)] exp
@@ -31,7 +32,6 @@
          
          [(['define target a_binding] :seq)] `(def ~target (scheme->cljs ~a_binding)) 
 
-
          [(['cond & conditions] :seq)]  (cons `cond
                                               (mapcat
                                                 (fn [[cnd op]]
@@ -39,12 +39,13 @@
                                                     [:else `(scheme->cljs ~op)]
                                                      `[(scheme->cljs ~cnd) (scheme->cljs ~op)]))
                                                 conditions))
+
+         [(['begin & exprs] :seq)] (cons `do (scheme-body->cljs exprs))
+
          [(['quote an_exp] :seq)] `'~an_exp
-
+         
          [([proc & args] :seq)] (cons `(scheme->cljs ~proc)  (scheme-body->cljs args))
-
-        
-
+         
          [([] :seq)] nil
 
-         [:default] :error))
+         [:default] `(:error in ~exp)))
