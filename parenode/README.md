9e8ece3466b69307752217a2ce21b7ef2d66f5e4
Rafik Naccache <rafik@tnteam.rocks>
2015-12-05 10:47:39 +0000

    Initial parenode commit

---
+++ parenode/README.md
@@ -0,0 +1,16 @@
+# parenode
+
+A Simple Scheme Implementation that Spits JavaScript.
+
+Cosntrcuts ASTs compatible to the Mozilla Parser API, then transformed
+to JavaScript Using ESCodeGen.
+
+## Usage
+
+TOD
+
+## License
+
+Copyright © 2015 The parenode team.
+
+Distributed under the BSD 3 Clause License.
---
+++ parenode/project.clj
@@ -0,0 +1,6 @@
+(defproject parenode "0.1.0-SNAPSHOT"
+  :description "parenode : Scheme for JavaScript - Clojure Cup 2015 by TNTeam"
+  :url "http://clojurecup.parenode.org"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
