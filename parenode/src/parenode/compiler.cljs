8ad0fcfc58abb6e31f4b8f957b75c1cfc63140c9
Rafik Naccache <rafik@tnteam.rocks>
2015-12-05 14:13:52 +0000

    fixed other conditions in match

--- parenode/src/parenode/compiler.cljs
+++ parenode/src/parenode/compiler.cljs
@@ -13,6 +13,8 @@
                                                          body))
          [(['define target binding] :seq)] `(def ~target (scheme->cljs ~binding)) 
          [(['if cond do_true do_false] :seq)] `(if (scheme->cljs @cond) (scheme->cljs ~do_true) (scheme->cljs ~do_false))
+         [(['quote rest] :seq)] rest
          [([proc & args] :seq)] (cons `(scheme->cljs ~proc)  (map (fn [arg] `(scheme->cljs ~arg)) args))
+        
          [a_symbol]  a_symbol
          [:default] :error))
