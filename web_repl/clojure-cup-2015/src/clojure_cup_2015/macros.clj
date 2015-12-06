3b3df305e1ecc0413f7f1801b170f3d0b55c2820
Rafik NACCACHE <rafik@tnteam.rocks>
2015-12-06 14:39:58 +0000

    Update macros.clj

--- web_repl/clojure-cup-2015/src/clojure_cup_2015/macros.clj
+++ web_repl/clojure-cup-2015/src/clojure_cup_2015/macros.clj
@@ -112,7 +112,7 @@
         pattern-rows (define-syntax-matches literals  pattern-templates)]
     `(defn ~macro-name
       [& ~input]
-      (let [input-lit-kws# (clojure-cup-2015.compiler/scheme-literals->keywords ~literals ~input) ]
+      (let [input-lit-kws# (clojure-cup-2015.core/scheme-literals->keywords ~literals ~input) ]
         (cljs.core.match/match [input-lit-kws#]
                ~@pattern-rows)))))
 
